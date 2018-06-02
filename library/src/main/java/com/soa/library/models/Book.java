15cb0150833d9a90eb04d633032d4b0f3900129e
kajtazovai <kajtazova@gmail.com>
2018-06-02 12:09:32 +0200

    Create model Book

--- library/src/main/java/com/soa/library/models/Book.java
+++ library/src/main/java/com/soa/library/models/Book.java
@@ -1,5 +1,63 @@
 package com.soa.library.models;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Book {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public int Id;
+	public String Title;
+	public String Genre;
+	public String Author;
+	public float Price;
+	public Book(int id, String title, String genre, String author, float price) {
+		super();
+		Id = id;
+		Title = title;
+		Genre = genre;
+		Author = author;
+		Price = price;
+	}
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getTitle() {
+		return Title;
+	}
+	public void setTitle(String title) {
+		Title = title;
+	}
+	public String getGenre() {
+		return Genre;
+	}
+	public void setGenre(String genre) {
+		Genre = genre;
+	}
+	public String getAuthor() {
+		return Author;
+	}
+	public void setAuthor(String author) {
+		Author = author;
+	}
+	public float getPrice() {
+		return Price;
+	}
+	public void setPrice(float price) {
+		Price = price;
+	}
+	
+	
+	
+	
+	
+	
 
 }
